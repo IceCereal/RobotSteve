6d41717cbedb923ed5a7174905d0a2a1a1fa04e5
Ananta Srikar <srikarananta01@gmail.com>
2020-09-30 22:17:06 +0530

    Changed to embedded messages and improved outputs

--- utils/random/fats.py
+++ utils/random/fats.py
@@ -14,22 +14,54 @@ class Fats(commands.Cog):
 	)
 	async def fats(self, ctx):
 		
-		message = ''
 		user = ctx.message.author
 
 		rand_nos = []
 
 		for i in range(4):
-			rand_nos.append(randint(0, 100))
+			
+			random_number = randint(0, 100)
+			
+			if i == 0:
 
-		message += "username is {}% fat.\n".format(rand_nos[0])
-		message += "username is really feeling {}% lonely right now.\n".format(rand_nos[1])
-		message += "username eats {}kg of food every day.\n".format(rand_nos[2] / 5)
-		message += "username is indeed, {}% crap at Minecraft.".format(rand_nos[3])
+				if random_number < 30:
+					rand_nos.append('Not')
+				
+				elif random_number > 30 and random_number < 70:
+					rand_nos.append('Mildly')
 
-		message.replace('username', user.mention)
+				else:
+					rand_nos.append('Very')
 
-		await ctx.channel.send(message)
+			elif i == 1:
+
+				if random_number < 30:
+					rand_nos.append('Not feeling')
+				
+				elif random_number > 30 and random_number < 70:
+					rand_nos.append('Mildly')
+
+				else:
+					rand_nos.append('Really')
+
+			elif i == 2:
+				rand_nos.append(random_number / 5)
+
+			elif i == 3:
+				rand_nos.append(round(random_number / 10))
+
+		rgb_values = [255, 173, 51]
+
+		title = "{}: Stats".format(user)
+
+		embed = discord.Embed(title=title, colour=discord.Colour.from_rgb(*rgb_values))
+
+		embed.add_field(name="Fatness", value='{} fat'.format(rand_nos[0]), inline=True)
+		embed.add_field(name="Lonliness", value='{} lonely'.format(rand_nos[1]), inline=True)
+		embed.add_field(name="Consumption of Food", value='{}kg'.format(rand_nos[2]), inline=True)
+		embed.add_field(name="Crappiness at Minecraft", value='{}/10 bad at the game'.format(rand_nos[3]), inline=True)
+
+		await ctx.channel.send(embed = embed)
 
 
 def setup(bot):
